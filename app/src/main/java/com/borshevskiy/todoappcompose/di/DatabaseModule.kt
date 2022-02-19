e346191b81636a890b8ae10b28f99bd2b266b302
borshevskiy <sk8er1001@mail.ru>
2022-02-19 22:17:11 +0600

    Setup Dagger-Hilt

---
+++ app/src/main/java/com/borshevskiy/todoappcompose/di/DatabaseModule.kt
@@ -0,0 +1,27 @@
+package com.borshevskiy.todoappcompose.di
+
+import android.content.Context
+import androidx.room.Room
+import com.borshevskiy.todoappcompose.data.ToDoDatabase
+import com.borshevskiy.todoappcompose.utils.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object DatabaseModule {
+
+    @Singleton
+    @Provides
+    fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context, ToDoDatabase::class.java, DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideDAO(database: ToDoDatabase) = database.toDoDao()
+
+}
